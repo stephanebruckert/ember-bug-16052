8bbacf9136a755a5fb87827e5b87da1b8ec61e80
Stephane Bruckert <contact@stephanebruckert.com>
2018-01-16 20:17:18 +0000

    'this' available from tests

--- app/components/show-dummy/component.js
+++ app/components/show-dummy/component.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
+import Ember from 'ember';
+
+const { computed } = Ember;
+
 export default Component.extend({
+  dummies: computed.filter('model', function() {
+    debugger
+    return this.get('model');
+  })
 });
--- tests/integration/components/show-dummy/component-test.js
+++ tests/integration/components/show-dummy/component-test.js
@@ -1,24 +1,22 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import startMirage from '../../../helpers/start-mirage';
 
 moduleForComponent('show-dummy', 'Integration | Component | show dummy', {
-  integration: true
+  integration: true,
+  setup() {
+    this.server = startMirage();
+  },
+  afterEach() {
+    server.shutdown();
+  }
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  let dummies = server.createList('dummy', 3);
+  this.set('dummies', dummies);
 
-  this.render(hbs`{{show-dummy}}`);
+  this.render(hbs`{{show-dummy model=dummies}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#show-dummy}}
-      template block text
-    {{/show-dummy}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'There are 3 dummies');
 });
